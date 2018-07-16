27c1a84ecfc638e3937fd198d809fcf5dc9d5eca
kiyo_yodo <kiyoshibudokan@hotmail.com>
2018-07-16 02:00:40 -0300

    Primeiro projeto React - add componente

--- src/meuComponente.js
+++ src/meuComponente.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 
-class meuComponente extends Component {
+class MeuComponente extends Component {
   render() {
     return (
       <div className="component">
-        <h1>
-          Este é um componente dentro de outro
-        </h1>
+        <h1>Este é um componente dentro de outro</h1>
+        /*passando o props*/
+        <h2>{this.props.propTitulo}</h2>
+        <h2>{this.props.propNome}</h2>
+        <div onClick={this.props.propClicou}>Me clique</div>
+        /*passando o props - FIM*/
       </div>
     );
   }
 }
 
-export default meuComponente;
+export default MeuComponente;
